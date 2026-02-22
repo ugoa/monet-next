150afcb15a01443adb1d1f2b96e7142838c671fa
Dawei Hu <hoodavy@gmail.com>
2026-02-22 19:44:38 +0800

    async without structured concurrency

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,17 @@ use bytes::Bytes;
 use http_body_util::Full;
 use hyper::{Method, Request, Response, StatusCode, body::Incoming};
 use hyper::{server::conn::http1, service::service_fn};
+use std::cell::RefCell;
+use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
 use crate::rt::HyperStream;
 
-async fn action(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+async fn action(
+    req: Request<Incoming>,
+    cache: &RefCell<HashMap<&'static str, f32>>,
+) -> Result<Response<Full<Bytes>>, Infallible> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => Ok(Response::new(Full::new(Bytes::from("Hello World!")))),
         (&Method::GET, "/compio") => Ok(Response::new(Full::new(Bytes::from("Hello Compio!")))),
@@ -27,14 +32,21 @@ fn main() {
         let addr: SocketAddr = ([0, 0, 0, 0], port).into();
         let listener = compio::net::TcpListener::bind(addr).await.unwrap();
 
+        let cache = RefCell::new(HashMap::from([
+            ("Mercury", 0.4),
+            ("Venus", 0.7),
+            ("Earth", 1.0),
+            ("Mars", 1.5),
+        ]));
+
         loop {
             let (io, _) = listener.accept().await.unwrap();
 
             let io = HyperStream::new(io);
 
-            compio::runtime::spawn(async move {
+            compio::runtime::spawn(async {
                 if let Err(err) = http1::Builder::new()
-                    .serve_connection(io, service_fn(action))
+                    .serve_connection(io, service_fn(async |req| action(req, &cache).await))
                     .await
                 {
                     println!("Error serving connection: {:?}", err);
