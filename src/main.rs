955e056d0fe38b4a971a991d8c8e37e39538eb43
Dawei Hu <hoodavy@gmail.com>
2026-02-22 13:57:40 +0800

    mininum compio-hyper http server

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+mod rt;
+
+use bytes::Bytes;
+use http_body_util::Full;
+use hyper::{Method, Request, Response, StatusCode, body::Incoming};
+use hyper::{server::conn::http1, service::service_fn};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use crate::rt::HyperStream;
+
+async fn action(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(Full::new(Bytes::from("Hello World!")))),
+        (&Method::GET, "/compio") => Ok(Response::new(Full::new(Bytes::from("Hello Compio!")))),
+        _ => Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Full::new(Bytes::from("404 not found")))
+            .unwrap()),
+    }
+}
+
+fn main() {
+    println!("Running http server on 0.0.0.0:23300");
+    let body = async {
+        let addr: SocketAddr = ([0, 0, 0, 0], 23300).into();
+        let listener = compio::net::TcpListener::bind(addr).await.unwrap();
+
+        loop {
+            let (io, _) = listener.accept().await.unwrap();
+
+            let io = HyperStream::new(io);
+
+            compio::runtime::spawn(async move {
+                if let Err(err) = http1::Builder::new()
+                    .serve_connection(io, service_fn(action))
+                    .await
+                {
+                    println!("Error serving connection: {:?}", err);
+                }
+            })
+            .detach();
+        }
+    };
+    compio::runtime::Runtime::new().unwrap().block_on(body);
+}
---
+++ src/rt.rs
@@ -0,0 +1,126 @@
+use compio::{
+    io::{AsyncRead, AsyncWrite, compat::AsyncStream},
+    net::{TcpListener, TcpStream, UnixListener, UnixStream},
+};
+use send_wrapper::SendWrapper;
+use std::{
+    future::{Future, IntoFuture, poll_fn},
+    io,
+    marker::PhantomData,
+    net::SocketAddr,
+    ops::DerefMut,
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll, ready},
+    time::Duration,
+};
+
+/// Types that can listen for connections.
+pub trait Listener: 'static {
+    /// The listener's IO type.
+    type Io: AsyncRead + AsyncWrite + Unpin + 'static;
+
+    /// The listener's address type.
+    type Addr;
+
+    /// Accept a new incoming connection to this listener.
+    ///
+    /// If the underlying accept call can return an error, this function must
+    /// take care of logging and retrying.
+    fn accept(&mut self) -> impl Future<Output = (Self::Io, Self::Addr)>;
+
+    /// Returns the local address that this listener is bound to.
+    fn local_addr(&self) -> io::Result<Self::Addr>;
+}
+
+impl Listener for TcpListener {
+    type Addr = SocketAddr;
+    type Io = TcpStream;
+
+    async fn accept(&mut self) -> (Self::Io, Self::Addr) {
+        loop {
+            match Self::accept(self).await {
+                Ok(tup) => return tup,
+                Err(_e) => todo!(), // handle error
+            }
+        }
+    }
+
+    fn local_addr(&self) -> io::Result<Self::Addr> {
+        Self::local_addr(self)
+    }
+}
+
+impl Listener for UnixListener {
+    type Addr = socket2::SockAddr;
+    type Io = UnixStream;
+
+    async fn accept(&mut self) -> (Self::Io, Self::Addr) {
+        loop {
+            match Self::accept(self).await {
+                Ok(tup) => return tup,
+                Err(_e) => todo!(), // handle error
+            }
+        }
+    }
+
+    fn local_addr(&self) -> io::Result<Self::Addr> {
+        Self::local_addr(self)
+    }
+}
+
+/// A stream wrapper for hyper.
+pub struct HyperStream<S>(SendWrapper<AsyncStream<S>>);
+
+impl<S> HyperStream<S> {
+    /// Create a hyper stream wrapper.
+    pub fn new(s: S) -> Self {
+        Self(SendWrapper::new(AsyncStream::new(s)))
+    }
+
+    /// Get the reference of the inner stream.
+    pub fn get_ref(&self) -> &S {
+        self.0.get_ref()
+    }
+}
+
+impl<S> std::fmt::Debug for HyperStream<S> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("HyperStream").finish_non_exhaustive()
+    }
+}
+
+impl<S: AsyncRead + Unpin + 'static> hyper::rt::Read for HyperStream<S> {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        mut buf: hyper::rt::ReadBufCursor<'_>,
+    ) -> Poll<io::Result<()>> {
+        let stream = unsafe { self.map_unchecked_mut(|this| this.0.deref_mut()) };
+        let slice = unsafe { buf.as_mut() };
+        let len = ready!(stream.poll_read_uninit(cx, slice))?;
+        unsafe { buf.advance(len) };
+        Poll::Ready(Ok(()))
+    }
+}
+
+impl<S: AsyncWrite + Unpin + 'static> hyper::rt::Write for HyperStream<S> {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<io::Result<usize>> {
+        let stream = unsafe { self.map_unchecked_mut(|this| this.0.deref_mut()) };
+        futures_util::AsyncWrite::poll_write(stream, cx, buf)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        let stream = unsafe { self.map_unchecked_mut(|this| this.0.deref_mut()) };
+        futures_util::AsyncWrite::poll_flush(stream, cx)
+    }
+
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        let stream = unsafe { self.map_unchecked_mut(|this| this.0.deref_mut()) };
+        futures_util::AsyncWrite::poll_close(stream, cx)
+    }
+}
